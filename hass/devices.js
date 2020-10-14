4dc2e8eb487605c0941f840baf36ce55803dfe8c
Homected <support@homected.com>
2020-10-14 17:31:42 +0200

    Update devices.js
    Added Fibaro FGR-223 Roller Shutter 3

--- hass/devices.js
+++ hass/devices.js
@@ -340,6 +340,7 @@ module.exports = {
   '152-263-25601': [THERMOSTAT_2GIG], // Radio Thermostat / 2GIG CT100
   '152-256-8194': [THERMOSTAT_2GIG], // Radio Thermostat / 2GIG CT32
   '271-4096-770': [COVER], // Fibaro FGRM222
+  '271-4096-771': [COVER], // Fibaro FGR223
   '328-1-1': [STELLA_ZWAVE],
   '328-1-3': [SPIRIT_ZWAVE_PLUS],
   '328-2-3': [SPIRIT_ZWAVE_PLUS],
